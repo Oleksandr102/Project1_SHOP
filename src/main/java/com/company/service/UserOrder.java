f59c63e66b803d09b44f80a7c07b410adaf5b104
Volodymyr Kliuiko <wantos@ukr.net>
2020-07-25 22:34:18 +0200

    Make User order

--- src/main/java/com/company/service/UserOrder.java
+++ src/main/java/com/company/service/UserOrder.java
@@ -1,4 +1,46 @@
-package main.java.com.company.service;
+package com.company.service;
 
-public class UserOrder {
+enum Categories {
+    PHONE, TV, LAPTOP
+}
+
+public class UserOrder implements OrderFromUser {
+    private String activeOrder;
+    private Categories categories;
+
+    private ActiveOrders activeOrders;
+
+
+    public UserOrder(String activeOrder, Categories categories) {
+        this.activeOrder = activeOrder;
+        this.categories = categories;
+    }
+
+    public String getActiveOrder() {
+        return activeOrder;
+    }
+
+    public void setActiveOrder(String activeOrder) {
+        this.activeOrder = activeOrder;
+    }
+
+    public Categories getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Categories categories) {
+        this.categories = categories;
+    }
+
+    public void addOrder(String activeOrder) {
+        activeOrders.getOrderslist().put(categories, activeOrder);
+    }
+
+    public void displayProducts() {
+        System.out.println(activeOrders.getOrderslist());
+    }
+
+    public void searchByCategory(Categories categories) {
+        activeOrders.getOrderslist().forEach((Categories, String) -> System.out.println(Categories + " " + String));
+    }
 }
