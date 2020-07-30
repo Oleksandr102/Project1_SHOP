80516aec9739ef8954254108aec1f473c9632694
Artur Teben <teben.artur99@gmail.com>
2020-07-30 14:40:06 +0300

    Extend method toString

--- src/main/java/com/company/model/product/Product.java
+++ src/main/java/com/company/model/product/Product.java
@@ -57,10 +57,16 @@ public class Product {
 
     @Override
     public String toString() {
-        return " name=" + name +
-                " productСode=" + productСode +
-                " category=" + category +
-                " producer='" + producer +
-                " price=" + price;
+        String formName = String.format("Name = %-15s", name);
+        String formCode = String.format("Code = %-10s", productСode);
+        String formCategory = String.format("Category = %-20s", category);
+        String formProducer = String.format("Producer = %-10s", producer);
+        String formPrice = String.format("Price = %-5s", price);
+        return          formName +
+                " | " + formCode +
+                " | " + formCategory +
+                " | " + formProducer +
+                " | " + formPrice + "$";
     }
 }
+
