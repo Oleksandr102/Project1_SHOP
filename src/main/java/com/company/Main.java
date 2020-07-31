975c0c6037119c41c37f3a1beb496d84505494a7
Artur Teben <teben.artur99@gmail.com>
2020-07-31 22:42:02 +0300

    Add IOException throw

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -4,9 +4,11 @@ import main.java.com.company.model.product.Category;
 import main.java.com.company.model.product.ProductManager;
 import main.java.com.company.view.AdminMenu;
 
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ProductManager.printALlProduct();
         ProductManager.printByCategory(Category.MOBILE);
         AdminMenu adminMenuService = new AdminMenu();
