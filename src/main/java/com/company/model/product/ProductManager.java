05e2e392f35b49c12fccd77ea99f4bfd9b881375
Artur Teben <teben.artur99@gmail.com>
2020-07-31 22:42:31 +0300

    Add methods getById and deleteProduct

--- src/main/java/com/company/model/product/ProductManager.java
+++ src/main/java/com/company/model/product/ProductManager.java
@@ -48,18 +48,38 @@ public class ProductManager {
                 .build();
     }
 
+    public static Map<Integer, Product> getById(Integer idProduct) {
+        return products
+                .entrySet()
+                .stream()
+                .filter(map -> map.getKey().equals(idProduct))
+                .collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+    }
+
     public static void printALlProduct() {
         products.forEach((a, b) -> System.out.println("ID: " + a + " | " + b));
     }
 
     public static void printByCategory(Category inputCategory) {
         Map<Integer, Product> result = products
-                .entrySet().stream()
-                .filter(map -> map.getValue().getCategory().equals(inputCategory))
-                .collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+                                        .entrySet()
+                                        .stream()
+                                        .filter(map -> map.getValue().getCategory().equals(inputCategory))
+                                        .collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
 
         result.forEach((a, b) -> System.out.println("Id " + a + " " + b));
     }
 
+    public static void deleteProduct(Integer productId) {
+        products.remove(productId);
+    }
+
+    public static void printById(Integer id) {
+        Map<Integer, Product> result = products
+                .entrySet().stream()
+                .filter(map -> map.getKey().equals(id))
+                .collect(Collectors.toMap(map -> map.getKey(), map -> map.getValue()));
+        result.forEach((a, b) -> System.out.println("Id " + a + " " + b));
+    }
 }
 
