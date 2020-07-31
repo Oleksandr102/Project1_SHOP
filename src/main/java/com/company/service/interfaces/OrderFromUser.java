cee94ac12ee96415face0f06b55bfdb391509844
Oleksandr102 <65391094+Oleksandr102@users.noreply.github.com>
2020-07-31 21:53:25 +0300

    Update OrderFromUser.java

--- src/main/java/com/company/service/interfaces/OrderFromUser.java
+++ src/main/java/com/company/service/interfaces/OrderFromUser.java
@@ -1,7 +1,5 @@
 package com.company.service;
 
 public interface OrderFromUser {
-    void displayProducts();
-    void addOrder(String activeOrder);
-    void searchByCategory(Categories categories);
+
 }
