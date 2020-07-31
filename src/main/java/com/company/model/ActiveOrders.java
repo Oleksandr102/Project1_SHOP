66b0b544cb61899cc89caa1b3cf95ff1f32bcc9f
Oleksandr102 <65391094+Oleksandr102@users.noreply.github.com>
2020-07-31 21:51:58 +0300

    Update ActiveOrders.java

--- src/main/java/com/company/model/ActiveOrders.java
+++ src/main/java/com/company/model/ActiveOrders.java
@@ -3,30 +3,5 @@ package main.java.com.company.model;
 import java.util.TreeMap;
 
 public class ActiveOrders {
-    private Map<Categories, String> ordersList = new TreeMap<>();
-    private float price;
 
-    public Map<Categories, String> getOrdersList() {
-        return ordersList;
-    }
-
-    public void setOrdersList(Map<Categories, String> ordersList) {
-        this.ordersList = ordersList;
-    }
-
-    public float getPrice() {
-        return price;
-    }
-
-    public void setPrice(float price) {
-        this.price = price;
-    }
-
-    public void removeFromOrder(String activeOrder) {
-        ordersList.remove(activeOrder);
-    }
-
-    public void addOrder(Categories categories, String activeOrder) {
-        ordersList.put(categories, activeOrder);
-    }
 }
