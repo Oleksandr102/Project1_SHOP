539415cf931520ac588925a0c3d7ed103c420a01
Oleksandr102 <65391094+Oleksandr102@users.noreply.github.com>
2020-08-05 16:17:19 +0300

    Update Main.java

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -11,20 +11,20 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        ProductManager productManager = new ProductManager();
-        productManager.printAllProduct();
-        //productManager.printByCategory(Category.MOBILE);
-        //productManager.printById(2);
-        Users user = new Users("user1", "test", "test");
-        Map<Integer, Product> product = productManager.getById(2);
-        Map<Integer, Product> product2 = productManager.getById(4);
-
-        OrderService.addOrder(user, product.get(2));
-        OrderService.addOrder(user, product2.get(4));
-
-        OrderService.showOrdersByUser(user);
-        OrderService.confirmOrder(user, Status.BLOCKED);
-
-        System.out.println(OrderService.countSum(user));
+//        ProductManager productManager = new ProductManager();
+//        productManager.printAllProduct();
+//        //productManager.printByCategory(Category.MOBILE);
+//        //productManager.printById(2);
+//        Users user = new Users("user1", "test", "test");
+//        Map<Integer, Product> product = productManager.getById(2);
+//        Map<Integer, Product> product2 = productManager.getById(4);
+//
+//        OrderService.addOrder(user, product.get(2));
+//        OrderService.addOrder(user, product2.get(4));
+//
+//        OrderService.showOrdersByUser(user);
+//        OrderService.confirmOrder(user, Status.BLOCKED);
+//
+//        System.out.println(OrderService.countSum(user));
     }
 }
