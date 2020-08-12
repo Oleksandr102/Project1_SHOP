9b69153a73ffdfa6b94efa1c6e03c3465d6477bf
Artur Teben <teben.artur99@gmail.com>
2020-08-12 14:18:04 +0300

    Delete useless class

--- src/main/java/com/company/service/exceptions/LoginAlreadyInUseException.java
+++
@@ -1,7 +0,0 @@
-package com.company.service.exceptions;
-
-public class LoginAlreadyInUseException extends Exception {
-    public LoginAlreadyInUseException(String message) {
-        super(message);
-    }
-}
